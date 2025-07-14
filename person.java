e5b2c16796fab769e368de47a236bbcb94f4d462
LaxmiSabita <laxmisabita384@gmail.com>
2025-07-15 00:46:43 +0530

    Create person.java
    ● Create a class Person
    ● Add member variables name as String, age and salary as int
    ● Initialize the member variable along with declaration.
    ● Now put the previous Person class in a package com.anudip.learning
    ● Add a main method. Add a print message “Test Successful”.
    ● Run the class after compilation.
    ● Modify the classpaths to see the error messages on the console.

---
+++ person.java
@@ -0,0 +1,11 @@
+public class Person {
+    // Member variables
+    String name = "Anita";
+    int age = 28;
+    int salary = 50000;
+ // Main method
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println("Test Successful");
+    }
+}
